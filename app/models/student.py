04d818f4cc79f043bfacaf8d6e4ec3e25ec80afe
Parth Batra <parthbatra524@example.com>
2026-06-01 18:13:27 +0530

    Initial commit

--- app/models/student.py
+++ app/models/student.py
@@ -1,9 +1,37 @@
-from pydantic import BaseModel,Field
+from pydantic import BaseModel, Field, field_validator
 
 class Student(BaseModel):
     id: int
-    name: str=Field(min_length=3,max_length=50)
-    age: int=Field(ge=18,le=88)
+
+    name: str = Field(
+        min_length=3,
+        max_length=50
+    )
+
+    age: int
+
+    @field_validator("name")
+    @classmethod
+    def validate_name(cls, value):
+
+        if not value.isalpha():
+            raise ValueError(
+                "Only letters allowed"
+            )
+
+        return value
+
+    @field_validator("age")
+    @classmethod
+    def validate_age(cls, value):
+
+        if value > 100 or value < 0:
+            raise ValueError(
+                "Invalid age"
+            )
+
+        return value
+
 
 students = [
     {"id": 1, "name": "Parth", "age": 20},
