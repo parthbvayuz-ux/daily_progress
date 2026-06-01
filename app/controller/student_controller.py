45c35fadbfe9a2b14fd041542299ffbfd230b84f
Parth Batra <parthbatra524@example.com>
2026-06-01 17:41:34 +0530

    Initial commit

--- app/controller/student_controller.py
+++ app/controller/student_controller.py
@@ -6,3 +6,55 @@ class StudentController:
     @staticmethod
     def get_all_students():
         return students
+    
+    @staticmethod
+    def add_student(student):
+        for s in students:
+            if s["id"] == student.id:
+                raise HTTPException(
+                    status_code=400,
+                    detail="Student ID already exists"
+                )
+
+        students.append(student.dict())
+
+        return {
+            "message": "Student added successfully",
+            "student": student
+            }
+    @staticmethod
+    def update_student(student_id: int, updated_student):
+        for index, student in enumerate(students):
+
+            if student["id"] == student_id:
+
+                students[index] = updated_student.dict()
+
+                return {
+                    "message": "Student updated successfully",
+                    "student": updated_student
+                }
+
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found"
+        )
+    
+    @staticmethod
+    def delete_student(student_id: int):
+
+        for index, student in enumerate(students):
+
+            if student["id"] == student_id:
+
+                deleted_student = students.pop(index)
+
+                return {
+                    "message": "Student deleted successfully",
+                    "student": deleted_student
+                }
+
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found"
+        )
--- app/views/student_view.py
+++ app/views/student_view.py
@@ -11,3 +11,23 @@ def home():
 @router.get("/students")
 def get_students():
     return StudentController.get_all_students()
+
+@router.post("/students")
+def add_student(student: Student):
+    return StudentController.add_student(student)
+
+@router.put("/students/{student_id}")
+def update_student(
+    student_id: int,
+    updated_student: Student
+):
+    return StudentController.update_student(
+        student_id,
+        updated_student
+    )
+
+@router.delete("/students/{student_id}")
+def delete_student(student_id: int):
+    return StudentController.delete_student(
+        student_id
+    )
